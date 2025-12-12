c15a6da82dabc6b3b77b59b8089944b2518ceb4a
Abdul Wahab <fadsync.contact@gmail.com>
2025-12-12 10:09:18 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
 // 🔧 Change ONLY this when you upload a new APK
-const APK_URL = "https://fadsync.com/your-apk-link.apk";
+const APK_URL = "https://drive.usercontent.google.com/download?id=1Pl9Rcl0XDMe8lydjTQtuUwJorP3yiS-h&export=download&authuser=0&confirm=t&uuid=d719f4f3-245a-4ea8-b63c-ab9b5f4d0c55&at=ALWLOp4VOFbuM9RlKehCH-gGxcGv%3A1765515321380";
